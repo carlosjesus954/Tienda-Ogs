76bca2e997a167d6c513825feec04eea61fa2677
Carlos Jesús Morales pica <carlosjesus.moralespica2000@gmail.com>
2023-04-09 21:40:01 +0200

    Fixeando un problema que habia de responsive + footer terminado con los enlaces a la pagina real con un target blank para que nunca se le salga de mi pagina

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,3 +1,30 @@
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+
 export const Footer = () => {
-  return <div>footer</div>;
+  const { footerLeft, footerRight } = useContext(AuthContext);
+  return (
+    <footer className="Footer Wrapper">
+      <div className="Footer-global">
+        <div className="Footer-container">
+          {footerLeft.map((element) => (
+            <span key={element.id} className="Footer-copy">
+              {" "}
+              {element.title}{" "}
+            </span>
+          ))}
+        </div>
+        <ul className="Footer-ul">
+          {footerRight.map((element) => (
+            <li className="Footer-li" key={element.id}>
+              <a href={element.href} className="Footer-enlace" target="_blank">
+                {" "}
+                {element.title}{" "}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
 };
--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -85,11 +85,72 @@ const mainConjuntos2 = [
     to: "accesorios",
   },
 ];
+const footerLeft = [
+  {
+    id: 1,
+    title: "@2023 Reserva todos los derechos",
+  },
+];
+const footerRight = [
+  {
+    id: 2,
+    title: "Política de envíos",
+    href: "https://www.ogsbrand.com/es/politica-de-envios/",
+  },
+  {
+    id: 3,
+    title: "Contacto",
+    href: "https://www.ogsbrand.com/es/contacto/",
+  },
+  {
+    id: 4,
+    title: "Condiciones de compra",
+    href: "https://www.ogsbrand.com/es/condiciones-de-compra/",
+  },
+  {
+    id: 5,
+    title: "Términos de servicio",
+    href: "https://www.ogsbrand.com/es/terminos-de-servicio/",
+  },
+  {
+    id: 6,
+    title: "Política de privacidad",
+    href: "https://www.ogsbrand.com/es/politica-de-privacidad/",
+  },
+  {
+    id: 7,
+    title: "Política de cookies",
+    href: "https://www.ogsbrand.com/es/politica-de-cookies/",
+  },
+  {
+    id: 8,
+    title: "Legal",
+    href: "https://www.ogsbrand.com/es/legal/",
+  },
+  {
+    id: 9,
+    title: "Tallas",
+    href: "https://www.ogsbrand.com/es/tallas/",
+  },
+  {
+    id: 10,
+    title: "Recomendaciones de cuidado",
+    href: "https://www.ogsbrand.com/es/care-recommendations/",
+  },
+];
 
 export const Provider = ({ children }) => {
   return (
     <AuthContext.Provider
-      value={{ navSup, navMid, navMid2, mainConjuntos, mainConjuntos2 }}
+      value={{
+        navSup,
+        navMid,
+        navMid2,
+        mainConjuntos,
+        mainConjuntos2,
+        footerLeft,
+        footerRight,
+      }}
     >
       {children}
     </AuthContext.Provider>
