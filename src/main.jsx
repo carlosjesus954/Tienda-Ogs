6807d29c0a640a0552c4043bce1506a881e5b876
Carlos Jesús Morales pica <carlosjesus.moralespica2000@gmail.com>
2023-06-12 12:52:00 +0200

    Optimizando

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom";
 import { GiftApp } from "./GiftApp";
 import { Provider } from "./context/Provider";
 
